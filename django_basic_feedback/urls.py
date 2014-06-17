10ca27c5824886fabe6a9dd74cf52284b3b34674
jon <jonmer85@gmail.com>
2014-06-16 23:02:34 -0400

    Django 1.6 and custom user model support

--- django_basic_feedback/urls.py
+++ django_basic_feedback/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #Copyright (C) 2011 Seán Hayes
 
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('django_basic_feedback.views',
 	url(r'^$', 'add', name="feedback"),
